88b57d96b26aa30fa9d291c3b657ca1941ac3386
onias <sayionias@gmail.com>
2021-12-20 02:15:33 +0200

    the switch statement again and again

--- basics/src/Basics.java
+++ basics/src/Basics.java
@@ -9,18 +9,23 @@ public class Basics {
         //n1.numChecker(12, 36);
         //using a switch statement
         Scanner sc= new Scanner(System.in);
-        System.out.println("enter your number");
-        int num = sc.nextInt();
-        switch (num){
-            case 0:
-                System.out.println("number is equal to 0");
+        System.out.println("enter your animal");
+        String  animal  = sc.next();
+        switch (animal.toUpperCase()){
+            case "DOGS":
+                System.out.println("domestic animal");
                 break;
-            case 1:
-                System.out.println("number is equal to 1");
+            case "CAT":
+                System.out.println("it is a domestic animal");
+                break;
+            case "TIGER":
+                System.out.println("it is a wild animal");
                 break;
             default:
-                System.out.println("number is either negative or, higher than 1");
+                System.out.println("unknown animal");
                 break;
+
+
         }
 
 
