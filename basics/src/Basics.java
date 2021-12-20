a310cb8815790f975db66c9c9360d4591aa09013
onias <sayionias@gmail.com>
2021-12-20 03:02:02 +0200

    demonstrated the while and the for loop

--- basics/src/Basics.java
+++ basics/src/Basics.java
@@ -8,32 +8,28 @@ public class Basics {
         //basicsSyntax n1 = new basicsSyntax();
         //n1.numChecker(12, 36);
         //using a switch statement
-        Scanner sc= new Scanner(System.in);
-        System.out.println("enter your animal");
-        String  animal  = sc.next();
-        switch (animal.toUpperCase()){
-            case "DOGS":
-                System.out.println("domestic animal");
-                break;
-            case "CAT":
-                System.out.println("it is a domestic animal");
-                break;
-            case "TIGER":
-                System.out.println("it is a wild animal");
-                break;
-            default:
-                System.out.println("unknown animal");
-                break;
+        /*Scanner sc= new Scanner(System.in);
+        System.out.println("enter your month");
+        String  month  = sc.next();
+        switch (month){
+            case JANUARY, JUNE, JULY -> 3;
+            case FEBRUARY, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER -> 1;
+            case MARCH, MAY, APRIL, AUGUST -> 2;
+            default -> 0;
 
-
+        };
+        for(int i = 1; i<= 10; i++){
+            System.out.println("say i love you babes");
+        }*/
+        int i = 1;
+        while(i<=5){
+            System.out.println("i miss Simbie");
+            i++;
         }
-
-
 
     }
 
-
-        }
+}
 
 
 
